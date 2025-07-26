0252755a4e2fdf17a8af38c42ce0de4260e04d30
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-07-26 18:14:10 +0300

    Add main routes to CreateBrowserRouter and Provides them to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import { createBrowserRouter } from "react-router";
+import LayoutPage from "./pages/LayoutPage";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { RouterProvider } from "react-router/dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <LayoutPage />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "products", element: <ProductsPage /> },
+        { path: "*", element: <NotFoundPage /> },
+      ],
+    },
+  ]);
   return (
     <>
-      <div className="bg-amber-600 text-white">Gallery Products</div>
+      <RouterProvider router={router} />
     </>
   );
 }
