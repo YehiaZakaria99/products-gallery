2283385f71fee61a1e9f8bfb179f71355a5944f7
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-07-26 20:27:13 +0300

    Add Navbar To Layout

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "./../components/Navbar";
 
 export default function LayoutPage() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
-  )
+  );
 }
