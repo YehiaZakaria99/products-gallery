7aa1a973cd9c23edd22f2a2780d650e9e10e03a6
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-07-27 19:40:19 +0300

    Update Some Style and add toggle theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,77 +1,87 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { cn } from "../../lib/utils";
 import { NavLink } from "react-router";
 import { ListIcon, XIcon } from "@phosphor-icons/react";
 import NavbarMobile from "./NavbarMobile";
+import logo from "../../assets/navbar/logo.webp";
+import ThemeToggle from "./../ThemeToggle/ThemToggle";
+
+const navItems = [
+  { name: "Home", to: "" },
+  { name: "Products", to: "products" },
+];
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 10);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+
+  const handleScroll = useCallback(() => {
+    setIsScrolled(window.scrollY > 10);
   }, []);
 
-  const navItems = [
-    { name: "Home", to: "" },
-    { name: "Products", to: "products" },
-  ];
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   return (
-    <>
-      <nav
-        className={cn(
-          "fixed top-0 left-0 w-full z-[9999999999999999999] transition-all duration-300",
-          isScrolled
-            ? "bg-white backdrop-blur-md shadow-md py-3"
-            : "bg-white py-5 shadow-xl"
-        )}
-      >
-        <div className="container flex items-center justify-between flex-wrap px-10">
-          {/* Logo */}
-          <NavLink to="" className="flex items-center z-50 ">
-            {/* <img src={logo} alt="Logo" className="md:h-10 h-6 w-auto" /> */}
-            <h3 className="md:h-10 h-6 w-auto">Logo</h3>
-          </NavLink>
+    <nav
+      className={cn(
+        "fixed top-0 left-0 w-full z-[9999] transition-all duration-300",
+        "bg-navbar",
+        isScrolled ? "backdrop-blur-md shadow-md py-3" : "py-5 shadow-xl"
+      )}
+    >
+      <div className="container flex items-center justify-between flex-wrap px-10">
+        {/* Logo */}
+        <NavLink to="/" className="flex items-center z-50">
+          <img
+            src={logo}
+            alt="Company Logo"
+            className="h-[60px] object-contain rounded-full"
+          />
+        </NavLink>
 
-          {/* Desktop */}
-          <div className="hidden lg:flex space-x-8 text-lg">
-            {navItems.map((item, index) => (
-              <div key={index} className="relative">
-                <NavLink
-                  aria-label={item.name}
-                  to={item.to}
-                  className="text-primary font-bold hover:text-hover transition-colors duration-300 flex items-center"
-                >
-                  {item.name}
-                </NavLink>
-              </div>
-            ))}
-          </div>
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex space-x-8 text-lg">
+          {navItems.map((item) => (
+            <NavLink
+              key={item.to}
+              to={item.to}
+              className={cn(
+                "text-textColor font-bold",
+                "dark:text-white",
+                "transition-colors duration-300 hover:text-main"
+              )}
+            >
+              {item.name}
+            </NavLink>
+          ))}
+        </div>
 
-          {/* Mobile */}
+        <div className="flex items-center gap-2">
           {/* Mobile Menu Button */}
           <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onClick={() => setIsMenuOpen((prev) => !prev)}
             className="lg:hidden z-50 text-primary cursor-pointer"
             aria-label="Toggle Menu"
           >
             {isMenuOpen ? (
-              <XIcon size={48} color="#761d9f" />
+              <XIcon size={30} color="#EF4823" />
             ) : (
-              <ListIcon size={48} color="#761d9f" />
+              <ListIcon size={30} color="#EF4823" />
             )}
           </button>
+          {/* Theme Toggle */}
+          <ThemeToggle />
         </div>
+        {/* Mobile Menu */}
         <NavbarMobile
           navItems={navItems}
           isMenuOpen={isMenuOpen}
           setIsMenuOpen={setIsMenuOpen}
         />
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
--- src/components/Navbar/NavbarMobile.jsx
+++ src/components/Navbar/NavbarMobile.jsx
@@ -1,36 +1,48 @@
-import React, { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { cn } from "../../lib/utils";
 import { NavLink } from "react-router";
 
 export default function NavbarMobile({ navItems, isMenuOpen, setIsMenuOpen }) {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    }
+    if (isMenuOpen) {
+      document.addEventListener("pointerdown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("pointerdown", handleClickOutside);
+    };
+  }, [isMenuOpen, setIsMenuOpen]);
+
   return (
-    <>
-      {/* Mobile Menu */}
-      <div
-        className={cn(
-          "lg:hidden ease-in-out  text-green-800 px-5 rounded-md",
-          isMenuOpen
-            ? "max-h-[1000px] opacity-100 py-4 pointer-events-auto"
-            : "max-h-0 opacity-0 py-0 pointer-events-none",
-          "transition-all duration-300"
-        )}
-      >
-        <div className=" text-center text-lg font-bold text-primary">
-          {navItems.map((item, index) => (
-            <div className="relative" key={index}>
-              <NavLink
-                aria-label={item.name}
-                key={index}
-                to={item.to}
-                className="hover:text-hover transition duration-300 py-4 inline-flex"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                {item.name}
-              </NavLink>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+    <div
+      ref={menuRef}
+      role="menu"
+      // aria-hidden={!isMenuOpen}
+      className={cn(
+        "fixed top-0 left-0 w-full h-screen bg-navbar lg:hidden flex flex-col items-center justify-center transition-transform duration-300 ease-in-out z-40",
+        isMenuOpen
+          ? "translate-x-0 opacity-100"
+          : "-translate-x-100 opacity-0 pointer-events-none",
+          "text-center text-xl font-bold text-primary space-y-8"
+      )}
+    >
+        {navItems.map((item) => (
+          <NavLink
+            key={item.to}
+            aria-label={item.name}
+            to={item.to}
+            className="hover:text-hover transition duration-300 text-textColor"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            {item.name}
+          </NavLink>
+        ))}
+    </div>
   );
 }
